5c50ee322b2af18f9254c0950746bf316d3773a0
Priscillah Njai <priscillahnjaii@gmail.com>
2025-06-01 00:58:37 +0300

    Adds Customer and Table models

--- models.py
+++ models.py
@@ -3,3 +3,24 @@ from sqlalchemy.orm import relationship, declarative_base
 from datetime import datetime
 
 Base = declarative_base()
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    phone = Column(String)
+    email = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    reservations = relationship("Reservation", back_populates="customer")
+
+
+class Table(Base):
+    __tablename__ = 'tables'
+
+    id = Column(Integer, primary_key=True)
+    table_number = Column(Integer, unique=True)
+    capacity = Column(Integer)
+    is_available = Column(Boolean, default=True)
+
+    reservations = relationship("Reservation", back_populates="table")
