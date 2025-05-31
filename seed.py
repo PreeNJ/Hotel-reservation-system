3858888475eaeabc53b8e202a9a60ed52d6d0118
Priscillah Njai <priscillahnjaii@gmail.com>
2025-06-01 01:30:16 +0300

    Add initial customer data

--- seed.py
+++ seed.py
@@ -10,3 +10,7 @@ session = Session()
 # Drop and recreate tables
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+
+# Create customers
+c1 = Customer(name="Alice", phone="1234567890", email="alice@example.com")
+c2 = Customer(name="Bob", phone="0987654321", email="bob@example.com")
